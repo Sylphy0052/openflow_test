5bf74d30b9963c1fbb2117bfe0f6a4d46d33e1f6
KentoFuruhashi <al13095@shibaura-it.ac.jp>
2017-06-08 19:34:04 +0900

    miclone

---
+++ research/miclone.rb
@@ -0,0 +1,30 @@
+class Miclone < Controller
+
+    def start
+        puts "trema running..."
+        @dpid = []
+    end
+
+    def switch_ready datapath_id
+        puts "switch ready #{datapath_id.to_hex}"
+        @dpid.push(datapath_id)
+    end
+
+    def packet_in datapath_id, message
+        macsa = packet_in.macsa#source_mac_address
+        macda = packet_in.macda#destination_mac_address
+        ipsa = packet_in.ipv4_saddr#ipv4_source_address
+        ipda = packet_in.ipv4_daddr#ipv4_destination_address
+
+
+        if !ipsa.nil? && "0.0.0.0" != ipsa.to_s then
+          puts "----------------------"
+          puts "macsa : #{macsa}"
+          puts "macda : #{macda}"
+          puts "ipsa : #{ipsa.to_s}"
+          puts "ipda : #{ipda.to_s}"
+          puts "----------------------"
+
+        end
+    end
+end
